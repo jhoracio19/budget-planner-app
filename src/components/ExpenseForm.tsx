37f6b3a911d343f0a0bc834872f9aeb92c03edbe
Jose Horacio <jhoracio19@hotmail.com>
2026-06-07 22:11:40 -0600

    creando el formulario de registrar gasto

---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,74 @@
+import { categories } from "../data/categories";
+
+
+export default function ExpenseForm() {
+  return (
+    <form className="space-y-5">
+      <legend
+        className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
+      >Nuevo Gasto</legend>
+
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="expenseName"
+          className="text-xl"
+          >
+            Nombre Gasto:
+        </label>
+        <input 
+          type="text" 
+          id="expenseName"
+          placeholder="Añade el Nombre del Gasto"
+          className="bg-sky-100 p-2"
+          name="expenseName"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="amount"
+          className="text-xl"
+          >
+            Cantidad:
+        </label>
+        <input 
+          type="text" 
+          id="number"
+          placeholder="Añade la cantidad del gasto: ej. 300"
+          className="bg-sky-100 p-2"
+          name="amount"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="category"
+          className="text-xl"
+          >
+            Categoría:
+        </label>
+        <select 
+          id="category"
+          className="bg-sky-100 p-2"
+          name="category"
+        >
+          <option value="">-- Seleccione --</option>
+          {categories.map(category => (
+            <option
+              key={category.id} 
+              value={category.id}
+              >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input 
+        type="submit" 
+        className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+        value={'Registrar Gasto'}
+      />
+    </form>
+  )
+}
